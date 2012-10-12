936bd6607542b608131989efd614a21a793ed060
rafaferruz@hotmail.com <rafaferruz@hotmail.com@a31b6c38-498d-e236-bb34-240969b7799e>
2012-10-12 18:06:38 +0000

    Separar en directorios de versiones

---
+++ z01/libraries/Menux/Menux.cpp
@@ -0,0 +1,111 @@
+#include "Arduino.h"
+#include "MenuOption.h"
+#include "Menux.h"
+ 
+Menux::Menux() {
+ this->lastOption = 0;
+}
+
+void Menux::addMenuOption(MenuOption menuOption) {
+	menuOptionList[lastOption] = menuOption;
+	lastOption += 1;
+}	
+
+byte Menux::getLastOption() {
+	return lastOption;
+}
+
+byte Menux::getPresentOption(){
+	return presentOption;
+}
+
+void Menux::setPresentMenuCode(byte menuCode){
+	presentMenuCode = menuCode;
+}
+
+byte Menux::getPresentMenuCode(){
+	return presentMenuCode;
+}
+
+MenuOption Menux::getPresentMenuOption(){
+	return menuOptionList[presentOption];
+}
+
+void Menux::setTitleMenuOption(String title){
+	titleMenuOption = title;
+}
+
+String Menux::getTitleMenuOption(){
+	return titleMenuOption;
+}
+
+byte Menux::goBackMenu(byte indexOption){
+	if (menuOptionList[indexOption].getMenuBackCode() == 0) {
+		presentOption = searchNextOption( indexOption, 1);
+	} else {
+		presentOption = searchNextOption( indexOption, menuOptionList[indexOption].getMenuBackCode());
+	}
+	return presentOption;	
+}
+
+byte Menux::goNextOption(byte indexOption) {
+	// Se busca la siguiente MenuOption del grupo
+	presentOption = searchNextOption(indexOption, menuOptionList[indexOption].getMenuCode());
+	return presentOption;
+}
+
+// Devuelve el valor de la option
+String Menux::goSelectOption(byte indexOption) {
+	if (menuOptionList[indexOption].getMenuNextCode() == 0) {
+		String returnedValue;
+		returnedValue = menuOptionList[indexOption].getDefaultValue();
+		goBackMenu(indexOption);
+		return returnedValue;
+	} else {
+		presentOption = searchNextOption( indexOption, menuOptionList[indexOption].getMenuNextCode());
+		return "";
+	}
+}
+
+byte Menux::searchNextOption(byte fromOption, byte menuCodeToSearch) {
+	byte i;
+	for ( i = fromOption + 1; i < lastOption; i++) {
+		if (menuOptionList[i].getMenuCode() == menuCodeToSearch) {
+			presentMenuCode = menuOptionList[i].getMenuCode();
+			setTitleMenuOption(searchTitleMenuOption(i) + ": " + menuOptionList[fromOption].getDefaultValue());
+			return i;
+		}
+	}
+	for ( i = 0; i < fromOption; i++) {
+		if (menuOptionList[i].getMenuCode() == menuCodeToSearch) {
+			presentMenuCode = menuOptionList[i].getMenuCode();
+			setTitleMenuOption(searchTitleMenuOption(i) + ": " + menuOptionList[fromOption].getDefaultValue());
+			return i;
+		}
+	}
+	return fromOption;
+}
+
+void Menux::showMenuOption(LiquidCrystal &lcd) {
+	// Se envía al display el título del grupo de MenuOptions	
+	lcd.clear();
+	if (presentMenuCode == 1) {
+		titleMenuOption = "Menu General";
+	}
+	lcd.setCursor(0, 0);
+	lcd.print(titleMenuOption);
+	// Se envía al display la MenuOption actual
+	lcd.setCursor(0, 1);
+	lcd.print(menuOptionList[presentOption].getOptionText());
+	return;
+}
+
+String Menux::searchTitleMenuOption(byte indexOption){
+	byte i;
+	for ( i = 0; i < lastOption; i++) {
+		if (menuOptionList[i].getMenuCode() == menuOptionList[indexOption].getMenuBackCode()) {
+			return menuOptionList[i].getOptionText();
+		}
+	}
+	return "";
+}
