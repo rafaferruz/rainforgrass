087dac47161e44910cc50e7d5283be50487236ab
rafaferruz@hotmail.com <rafaferruz@hotmail.com@a31b6c38-498d-e236-bb34-240969b7799e>
2012-10-12 18:06:38 +0000

    Separar en directorios de versiones

---
+++ trunk/z01/rain_z01/constants.h
@@ -0,0 +1,36 @@
+/*
+constants.h
+
+Constantes utilizadas en el proyecto RAIN_Z01.
+*/
+
+// Definición de pines para uso del display LCD
+const byte LCD_COLUMNS = 16;
+const byte LCD_ROWS = 2;
+#define LCD_DB4 A0
+#define LCD_DB5 A1
+#define LCD_DB6 A2
+#define LCD_DB7 A3
+#define LCD_ENABLE A4
+#define LCD_REGISTER_SELECT A5
+
+// Definición de pines para uso del Keypad
+const byte KEYPAD_COLUMNS = 4;
+const byte KEYPAD_ROWS = 4;
+byte KEYPAD_ROW_PINS[KEYPAD_ROWS] = { 5, 4, 3, 2 };
+byte KEYPAD_COL_PINS[KEYPAD_COLUMNS] = { 9, 8, 7, 6 };
+String KEYPAD_USER_KEY_MAP = "123A456B789C#0*D";
+
+// Definiciones de acciones a ejecutar. El número entero es el que debe codificarse en el campo actionCode de MenuOption.
+const byte ACTION_ACTIVATE = 1;
+const byte ACTION_DEACTIVATE = 2;
+
+// Definición de parámetros de comunicaciones Serial
+const unsigned long SERIAL_SPEED = 9600;
+
+// Milisegundos entre consultas de estado de teclas
+const byte DELAY_CHECK_KEYS = 100;
+
+// Control del menú de la aplicación
+const byte MENU_START_CODE = 1;
+const String MENU_MAIN_TITLE = "RAIN_Z01";
