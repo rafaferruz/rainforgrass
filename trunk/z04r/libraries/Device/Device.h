9da32fa6b9bba910171da0a82386fb2f7e1729e5
rafaferruz@hotmail.com <rafaferruz@hotmail.com@a31b6c38-498d-e236-bb34-240969b7799e>
2012-11-04 10:28:26 +0000

    Primera versión del receptor

---
+++ trunk/z04r/libraries/Device/Device.h
@@ -0,0 +1,39 @@
+#ifndef Device_h
+#define Device_h
+
+#include "Arduino.h"
+#include "RainPComm.h"
+
+class Device {
+public:
+	Device();
+	Device(int id, int net, RainPComm * rp) ;
+ 
+	void initialize(int id, int  net);
+	void initialize(int id, int  net, RainPComm * rp);
+	int getDeviceId() ;
+	int getNetCode() ;
+	byte getState() ;
+	bool deactivate();
+	bool activate();
+	void setPinActivation(byte pinActivation);
+	void setDelayBetweenStates(unsigned int timeDelay);
+
+private:
+	int deviceId;
+	int netCode;
+	RainPComm * rp;
+	byte state;	// 0 = No activated; 1 = Activated;
+	unsigned long timeOfChange;
+	byte pinLevel;
+	byte pinActivation;
+	unsigned int delayBetweenStates;
+
+	bool isTimeOfChange( unsigned long time, unsigned int gapTime);
+	void setTimeOfChange( unsigned long time );
+	bool changePinLevel();
+	void setPinLevel(byte level);
+
+};
+ 
+#endif
